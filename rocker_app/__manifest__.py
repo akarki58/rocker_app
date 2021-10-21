75c82d0cd4ceb48e4fbd02c5859b430192c9f38b
Antti Kärki <antti.karki@iki.fi>
2021-10-21 13:45:12 +0300

    Add files via upload

--- rocker_app/__manifest__.py
+++ rocker_app/__manifest__.py
@@ -5,7 +5,7 @@
  'depends': ['base','web','mail'],
  'license': 'AGPL-3',
  'category': 'Extra Tools',
- 'version': '14.0.3.0.0',
+ 'version': '15.0.1.0.0',
  'data': [
   'security/rocker_security.xml',
   'security/ir.model.access.csv',
