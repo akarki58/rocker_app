24e9dfdf726f7eed47cffe50f88d80f72971a4d4
Antti Kärki <antti.karki@iki.fi>
2024-08-21 12:07:08 +0300

    Add files via upload

--- rocker_app/__manifest__.py
+++ rocker_app/__manifest__.py
@@ -3,7 +3,9 @@
  'description': 'Get data from Odoo & external PostgreSQL, SQLServer, MySQL, MariaDB, ODBC or Oracle databases.',
  'author': 'Antti Kärki',
  'depends': ['base', 'web', 'mail'],
- 'license': 'AGPL-3',
+ 'license': 'OPL-1',
+ 'price': 10.00,
+ 'currency': 'EUR',
  'category': 'Rocker/Reporting',
  'version': '17.0.1.1',
  'data': [
