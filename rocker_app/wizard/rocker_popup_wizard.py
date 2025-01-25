53991144a69fc3185af37f506d270ed9044dd11f
Akarki58 <antti.karki@iki.fi>
2025-01-25 18:22:33 +0200

    a

--- rocker_app/wizard/rocker_popup_wizard.py
+++ rocker_app/wizard/rocker_popup_wizard.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Copyright (C) 2019-Antti Kärki.
+#    Author: Antti Kärki.
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#
+# 2025-01-25
+#
+#############################################################################
 from odoo import api, fields, models, _
 
 class rocker_popup_wizard(models.TransientModel):
